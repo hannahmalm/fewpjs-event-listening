0ee61e76497ec0eb9d3ceec9d55d702780be32aa
Hannah Malm <hannahemalm616@gmail.com>
2022-02-01 18:00:48 -0600

    commit tree 7c0c2f9cbef323e2919b61f6cb71e6b04c6e957a

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById("input");
+
+function addingEventListener(e) {
+    input.addEventListener('click', function(e){
+        alert('I was clicked');
+    })
 }
